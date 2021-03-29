b42d3a574102a45309aa113d77aaa007057d638f
Dyln <Dyln.brewer23@gmail.com>
2021-03-28 20:17:24 -0500

    again

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ takeOff.addEventListener("click", function (event) {
     if (window.confirm("Confirm that the shuttle is ready for takeoff.")){
         flightStatus.innerHTML = "Shuttle in flight.";
         shuttleBackground.style.backgroundColor = "blue";
-        shuttleHeight = 10,000
+        shuttleHeight = "10,000"
     }
 });
 }
