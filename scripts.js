a05cb5ad3abf045164d29c516b9cb04e69f599fe
Dyln <Dyln.brewer23@gmail.com>
2021-03-28 20:00:46 -0500

    added shuttle height

--- scripts.js
+++ scripts.js
@@ -3,12 +3,14 @@
 function init() {
     const takeOff = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
-    const shuttleBackground = document.getElementById("shuttleBackground")
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const shuttleHeight = document.getElementById("spaceShuttleHeight");
 
 takeOff.addEventListener("click", function (event) {
     if (window.confirm("Confirm that the shuttle is ready for takeoff.")){
-        flightStatus.innerHTML = "Shuttle in flight."
-        shuttleBackground.style.backgroundColor = "blue"
+        flightStatus.innerHTML = "Shuttle in flight.";
+        shuttleBackground.style.backgroundColor = "blue";
+        shuttleHeight.style.height = 10,000
     }
 });
 }
