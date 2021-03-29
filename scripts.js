d32159a708dc84831e70a009316784cfd8617809
Dyln <Dyln.brewer23@gmail.com>
2021-03-28 19:38:33 -0500

    First part almost done

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+}
+takeOff.addEventListener("click", function (event) {
+    if (window.confirm("Confirm that the shuttle is ready for takeoff.")){
+        flightStatus.innerHTML = "Shuttle in flight."
+        shuttleBackground.style.backgroundColor = "blue"
+    }
+});
